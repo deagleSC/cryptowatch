e6fd5f6cc064ca673c16f15cb9a1b9ec7ef93950
DeagleOfficial <supratikofficial1@gmail.com>
2022-07-16 20:52:45 +0530

    Updates

---
+++ precache-manifest.185901b3883fdb517cc890d63617ce2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc440dca8af99b5c8406",
+    "url": "/cryptowatch/static/css/main.b52fa5af.chunk.css"
+  },
+  {
+    "revision": "fc440dca8af99b5c8406",
+    "url": "/cryptowatch/static/js/main.fc440dca.chunk.js"
+  },
+  {
+    "revision": "917a376cea4e6fe9ea8f",
+    "url": "/cryptowatch/static/js/1.917a376c.chunk.js"
+  },
+  {
+    "revision": "30efb4efb89977822559",
+    "url": "/cryptowatch/static/js/runtime~main.30efb4ef.js"
+  },
+  {
+    "revision": "032efaff90c01ed4573a708f94d09eaf",
+    "url": "/cryptowatch/index.html"
+  }
+];
